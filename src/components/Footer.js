b864444f565eb1328b73dbb0cd3166912f1aa880
Aida <kordi_aida43@yahoo.com>
2024-02-12 16:21:26 -0500

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,10 @@ import { FiFolder } from "react-icons/fi";
 import { FaLinkedin } from "react-icons/fa";
 import { ImGithub } from "react-icons/im";
 import { GoCodescan } from "react-icons/go";
+import Aboutme from "./components/pages/Aboutme";
+import Education from "./components/pages/Education";
+import Projects from "./components/pages/Projects";
+import Skills from "./components/pages/Skills";
 
 function Footer() {
   return (
