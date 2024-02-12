7934859ebc3cdfd94d06ae920aa413cb446a02f8
Aida <kordi_aida43@yahoo.com>
2024-02-12 15:11:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Menubar from "./components/Menubar";
+import Content from "./components/Content";
+import Sections from "./components/Sections";
+import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
@@ -7,20 +10,16 @@ import Projects from "./components/pages/Projects";
 import Skills from "./components/pages/Skills";
 import Education from "./components/pages/Education";
 import Aboutme from "./components/pages/Aboutme";
-import Content from "./components/Content";
-import Sections from "./components/Sections";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Router>
         <Menubar />
+        <Content />
+        <Sections />
+        <Footer />
         <Routes>
-          <Route path="/" exact Component={Content} />
-          <Route path="/" exact Component={Sections} />
-          <Route path="/" exact Component={Footer} />
-
           <Route path="/aboutme" Component={Aboutme} />
           <Route path="/education" Component={Education} />
           <Route path="/projects" Component={Projects} />
