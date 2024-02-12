16dd976af7d3cfa8b383f0b4d8b319b6a9db3d93
Aida <kordi_aida43@yahoo.com>
2024-02-12 15:56:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Menubar from "./components/Menubar";
+import Content from "./components/Content";
+import Sections from "./components/Sections";
+import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/pages/Home";
+
 import Projects from "./components/pages/Projects";
 import Skills from "./components/pages/Skills";
 import Education from "./components/pages/Education";
 import Aboutme from "./components/pages/Aboutme";
-import Content from "./components/Content";
-import Sections from "./components/Sections";
-import Footer from "./components/Footer";
 
 function App() {
   return (
