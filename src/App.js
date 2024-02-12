3123d6be87cc42779e80acfe32967db5c12c6912
Aida <kordi_aida43@yahoo.com>
2024-02-12 15:38:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import Projects from "./components/pages/Projects";
 import Skills from "./components/pages/Skills";
 import Education from "./components/pages/Education";
 import Aboutme from "./components/pages/Aboutme";
+import Content from "./Content";
+import Sections from "./Sections";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
       <Router>
         <Menubar />
+        <Content />
+        <Sections />
+        <Footer />
         <Routes>
-          <Route path="/" exact Component={Home} />
           <Route path="/aboutme" Component={Aboutme} />
           <Route path="/education" Component={Education} />
           <Route path="/projects" Component={Projects} />
